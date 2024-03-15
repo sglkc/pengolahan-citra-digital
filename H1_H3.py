12e5f291865bb5f461b0b8ffdd0d00de5fd7f678
Seya <31957516+sglkc@users.noreply.github.com>
2024-03-15 22:30:50 +0700

    feat(H1_H3): selesai H3 contour
    chore: rename thresholding -> segmentasi, dan submenu etce etcetect

--- H1_H3.py
+++ H1_H3.py
@@ -10,15 +10,17 @@ class H1_H3(G1):
     def __init__(self):
         super(H1_H3, self).__init__()
 
-        self.thMenu: QMenu
-        self.thGlobal: QMenu
-        self.thLocalAdapt: QMenu
+        self.sgMenu: QMenu
+        self.sgGlobalThres: QMenu
+        self.sgLocalAdaptThres: QMenu
+        self.sgContour: QMenu
 
-        self.thGlobal.triggered.connect(self.thGlobalTrigger)   # type: ignore
-        self.thLocalAdapt.triggered.connect(self.thLocalAdaptTrigger)   # type: ignore
+        self.sgGlobalThres.triggered.connect(self.sgGlobalThresTrigger)   # type: ignore
+        self.sgLocalAdaptThres.triggered.connect(self.sgLocalAdaptThresTrigger)   # type: ignore
+        self.sgContour.triggered.connect(self.contour)   # type: ignore
 
     @pyqtSlot(QAction)
-    def thGlobalTrigger(self, action: QAction):
+    def sgGlobalThresTrigger(self, action: QAction):
         if not hasattr(self, 'imageOriginal'):
             return self.showMessage('Error', 'Citra masih kosong!')
 
@@ -44,7 +46,7 @@ class H1_H3(G1):
         self.displayImage(2)
 
     @pyqtSlot(QAction)
-    def thLocalAdaptTrigger(self, action: QAction):
+    def sgLocalAdaptThresTrigger(self, action: QAction):
         if not hasattr(self, 'imageOriginal'):
             return self.showMessage('Error', 'Citra masih kosong!')
 
@@ -62,6 +64,18 @@ class H1_H3(G1):
             _, image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         else:
             image = cv2.adaptiveThreshold(image, 255, fungsi, cv2.THRESH_BINARY, 3, 2)
+
+        self.imageResult = image
+        self.displayImage(2)
+
+    @pyqtSlot()
+    def contour(self):
+        image = cv2.cvtColor(self.imageOriginal, cv2.COLOR_BGR2GRAY)
+        _, image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+        image = cv2.drawContours(image, contours, -1, (255, 0, 0), 2)
 
         self.imageResult = image
         self.displayImage(2)
